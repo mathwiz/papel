6b9879620d3a75f1f9dccca1753a44c71fbff15c
Yohan Lee <yohanlee@yahoo.com>
2021-02-04 13:17:30 -0500

    utils

--- molecularbio/code/__tmyutils.py
+++ molecularbio/code/__tmyutils.py
@@ -1,26 +1,45 @@
-import asciicode
+import myutils
 import unittest
 import random
 
-protein = "ACTIN"
-A = '1000001'
-C = '1000011'
-T = '1010100'
-I = '1001001'
-N = '1001110'
 
 class TestCase1(unittest.TestCase):
-    def test1(self):
-        coded = asciicode.binary_list(protein)
-        self.assertEqual(coded[0], A)
-        self.assertEqual(coded[1], C)
-        self.assertEqual(coded[2], T)
-        self.assertEqual(coded[3], I)
-        self.assertEqual(coded[4], N)
+    def test_counter(self):
+        count = myutils.counter(4)
+        self.assertEqual(next(count), 1)
+        self.assertEqual(next(count), 2)
+        self.assertEqual(next(count), 3)
+        self.assertEqual(next(count), 4)
+        with self.assertRaises(StopIteration) as context:
+            next(count)
 
-    def test2(self):
-        self.assertEqual(asciicode.binary(protein), "".join([A,C,T,I,N]))
 
+    def test_binary(self):
+        self.assertEqual('1', myutils.flip_binchar('0'))
+        self.assertEqual('0', myutils.flip_binchar('1'))
+        self.assertEqual('0', myutils.flip_binchar('anything'))
+   
+
+    def test_random_event(self):
+        probability = 0.2
+        limit = 100
+        count = 0
+        for x in range(0, limit):
+            if myutils.random_event(probability):
+                count += 1
+        print(count)
+        self.assertTrue(count > 0 and count < limit / 2)
+
+
+    def test_random_event2(self):
+        probability = 0.8
+        limit = 100
+        count = 0
+        for x in range(0, limit):
+            if myutils.random_event(probability):
+                count += 1
+        print(count)
+        self.assertTrue(count < limit and count > limit / 2)
 
 
 
--- molecularbio/code/myutils.py
+++ molecularbio/code/myutils.py
@@ -1,4 +1,14 @@
+import random
+
+
 def counter(limit=1000):
-    for x in range(0, limit):
+    for x in range(1, limit+1):
         yield x
 
+
+def flip_binchar(binarychar):
+    return '1' if binarychar == '0' else '0'
+
+
+def random_event(prob):
+    return random.random() < prob
